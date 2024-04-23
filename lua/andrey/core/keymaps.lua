13ff3b085f6e2e002f317c69da2886831f89a12b
Andrey Sadkov <ad.sadkov@gmail.com>
2024-04-23 08:15:26 +0300

    added: basic keymaps

---
+++ lua/andrey/core/keymaps.lua
@@ -0,0 +1,22 @@
+local keymap = vim.keymap -- for conciseness
+
+vim.g.mapleader = ","
+
+-- exit insertion mode with jk
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insertion mode with jk" })
+
+-- clearing the search result highlighting
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "cleaning the search result highlight" })
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+-- tabs management
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
